4ff76f0a4a9b4551c6e47beddf6dcde9831e7537
Vamsi <pusarlatarunvamsi@gmail.com>
2023-12-15 16:23:01 +0530

    updated Expenditure-analysis

--- easy/expenditure-analysis.js
+++ easy/expenditure-analysis.js
@@ -14,7 +14,21 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotals ={};
+  transactions.forEach(transaction => { 
+    const{category, price} = transaction;
+    if (categoryTotals[category]==undefined){
+      categoryTotals[category]=price;
+
+    }else{
+      categoryTotals[category]+=price;
+    }
+    
+  });
+  const result = Object.keys(categoryTotals).map(category => ({category , totalSpent: categoryTotals[category]}));
+  return result;
+
+ 
 }
 
 module.exports = calculateTotalSpentByCategory;
